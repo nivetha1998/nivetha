56c57aec4191bf04affd346a1e0c67e80b64b48c
nivetha1998 <50819369+nivetha1998@users.noreply.github.com>
2019-05-20 15:16:05 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,15 +3,18 @@ main()
 {
   char ch;
   scanf("%c",&ch);
+  if((ch>='a'&& ch<='z')||(ch>='A'&& ch<='Z')){
   if(ch=='a'|| ch=='e'|| ch =='i'|| ch=='o'|| ch=='u'|| ch=='A'|| ch== 'E'|| ch=='I'||ch=='O'||ch=='U')
   {
-    printf("vowel",ch);
+    printf("vowel");
   }
-  else if(isalpha(ch))                                                                                                                                                                                                                                                                                                                                                                                                    
+  else                                                                                                                                                                                                                                                                                                                                                                                                 
   {
-    printf("consonant",ch);
+    printf("consonant");
+  }
   }
   else
   {
-    printf("invalid",ch);
+    printf("invalid");
+}
 }
