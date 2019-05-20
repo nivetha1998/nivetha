44b0a8fbaa6cd25e9b79b8692f240140dc9560e7
nivetha1998 <50819369+nivetha1998@users.noreply.github.com>
2019-05-20 14:55:27 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -2,7 +2,7 @@
 main()
 {
   char ch;
-  scanf("%c",&ch)
+  scanf("%c",&ch);
   if(ch=='a'|| ch=='e'|| ch =='i'|| ch=='o'|| ch=='u'|| ch=='A'|| ch== 'E'|| ch=='I'||ch=='O'||ch=='U')
   {
     printf("vowel",ch);
