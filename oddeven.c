43d7f816a89f2a415fea9aeb87b1f4476282bff2
nivetha1998 <50819369+nivetha1998@users.noreply.github.com>
2019-05-20 13:13:16 +0530

    Update oddeven.c

--- oddeven.c
+++ oddeven.c
@@ -4,7 +4,9 @@ void main()
   int num;
   printf("enter the number\n");
   scanf("%d",&num);
-  if(num%2==0)
+  if(num<0)
+    printf("invalid");
+  else if(num%2==0)
     printf("even");
   else
     printf("odd");
