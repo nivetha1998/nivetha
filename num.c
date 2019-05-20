9e369196c92672c1aed690488917eb34acfc21b1
nivetha1998 <50819369+nivetha1998@users.noreply.github.com>
2019-05-20 12:30:08 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,10 @@
+
+  int n;
+  printf("enter a number \n");
+  scanf("%d",&n);
+if (n>0)
+  printf(" the number is positive");
+else if(n<0)
+  printf("the number is negative");
+else if(n==0)
+  printf("the number is zero");
