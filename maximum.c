72a5d0d8f7f2dcd78fe06799eae09b8a5da39f67
nivetha1998 <50819369+nivetha1998@users.noreply.github.com>
2019-05-21 06:36:59 -0700

    Create maximum.c

---
+++ maximum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int i,num,largest,a[30];
+scanf("%d",&num);
+for(i=0;i<num;i++)
+  scanf("%d",&a[i]);
+  largest=a[0];
+ for(i=0;i<num;i++)
+ {
+    if(a[i]>largest)
+    {
+    largest=a[i];
+    }
+ }
+ printf("%d",largest);
+	return 0;
