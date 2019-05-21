d3a6ae6435a21948ae6dc104241fe729a071d5f0
nivetha1998 <50819369+nivetha1998@users.noreply.github.com>
2019-05-20 20:51:00 -0700

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+{
+int n,i,flag=0;
+scanf("%d",&n);
+if(n<=1000)
+{
+for(i=2;i<=n/2;++i)
+{
+  if(n%i==0)
+  {
+    flag=1;
+    break;
+  }
+}
+  if(flag==0)
+  {
+    printf("yes");
+   }
+   else
+   {
+    printf("no");
+  }
+ }
+ else
+ printf("no");
+ }
