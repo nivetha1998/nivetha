96a5f815040ffe76922256fea617f29295d3cd3b
nivetha1998 <50819369+nivetha1998@users.noreply.github.com>
2019-05-21 06:20:59 -0700

    Create minimum.c

---
+++ minimum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int i,num,smallest,a[30];
+scanf("%d",&num);
+for(i=0;i<num;i++)
+  scanf("%d",&a[i]);
+  smallest=a[0];
+ for(i=0;i<num;i++)
+ {
+    if(a[i]<smallest)
+    {
+    smallest=a[i];
+    }
+ }
+ printf("%d",smallest);
+ }
