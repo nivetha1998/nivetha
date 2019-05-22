9827443d3f9fab953812a70a73216daf4a9271b9
nivetha1998 <50819369+nivetha1998@users.noreply.github.com>
2019-05-22 12:37:40 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+int i, j, temp, n, a[50],median;
+        scanf("%d", &n);
+        for (i = 1; i <=n; i++)
+            scanf("%d", &a[i]);
+ 
+        for (i = 1; i <=n; i++) 
+        {
+            for (j = i + 1; j <=n; j++)
+            {
+                if (a[i] > a[j]) 
+                {
+                    temp =  a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+ median=(n+1)/2;
+ printf("%d",a[median]);
