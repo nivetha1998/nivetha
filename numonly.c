90bf6aad8797b8b137ead2ec69c88fdb0744ff8e
nivetha1998 <50819369+nivetha1998@users.noreply.github.com>
2019-05-23 18:59:09 +0530

    Create numonly.c

---
+++ numonly.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+	char s[20],i;
+	scanf("%s",&s[i]);
+		if(s[i]>='0' && s[i]<='9')
+		{
+			printf("yes");
+		}
+		else
+		{
+			printf("No");
+		}
+	return 0;
+}
