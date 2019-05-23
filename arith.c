f9c2dacf137ca29c741d2ebd0a7b5c2c45140bf8
nivetha1998 <50819369+nivetha1998@users.noreply.github.com>
2019-05-23 19:24:08 +0530

    Update arith.c

--- arith.c
+++ arith.c
@@ -1,19 +1,8 @@
 #include<stdio.h>
 main()
 {
-int a,n,d,tn,sum=0;
+int a,n,d,sum=0;
 scanf("%d%d%d",&a,&n,&d);
 sum=(n*(2*a+(n-1)*d))/2;
-tn=a+(n-1)*d;
-for(i=a;i<=tn;i=i+d)
-{
-if(i!=tn)
-  {
-    printf("%d",i);
-   }
- else
- {
- printf("%d=%d",i,sum);
- }
- }
+ printf("%d",sum);
  }
