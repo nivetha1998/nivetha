9eb1399f45fa6a64081ad44526e6afe55f65e079
nivetha1998 <50819369+nivetha1998@users.noreply.github.com>
2019-05-23 14:22:07 +0530

    Update cntwrd.c

--- cntwrd.c
+++ cntwrd.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-main()
+void main()
 {
-    char s[200];
+    char s[100];
     int count = 0, i,
     scanf("%[^\n]s", s);
     for (i = 0;s[i] != '\0';i++)
     {
         if (s[i] == ' ')
-            count++;    
+        {
+            count++;
+        }
     }
     printf(" %d\n", count);
 }
